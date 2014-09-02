7aa302392ef5693098115f2cabfbd040bc46f6d9
Matt Schmulen <matt.schmulen@gmail.com>
2014-09-02 02:37:38 -0700

    added screen shot

--- sceneKitA/SceneKitA/ViewController.swift
+++ sceneKitA/SceneKitA/ViewController.swift
@@ -125,7 +125,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func render() {
